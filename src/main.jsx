7602556dab0469d7afa61ae77efee8afea2a022d
Saeidardalan <saeidardalan2@gmail.com>
2025-09-09 14:44:22 +0330

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,6 +0,0 @@
-const backendUrl = import.meta.env.VITE_BACKEND_URL;
-
-fetch(`${backendUrl}/commands`)
-  .then(res => res.json())
-  .then(data => console.log("✅ Commands:", data))
-  .catch(err => console.error("❌ Backend not reachable", err));
