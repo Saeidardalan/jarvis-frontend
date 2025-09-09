ca5e733f4181b6fdb1f638992d4456fb48a2dd54
Saeidardalan <saeidardalan2@gmail.com>
2025-09-09 13:00:40 +0330

    Delete src/components/ui/ui/ProtectedRoute.tsx

--- src/components/ui/ui/ProtectedRoute.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-import useAuthStore from "../store/auth-store";
-
-export default function ProtectedRoute({ children }) {
-  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
-  return isAuthenticated ? children : <Navigate to="/" />;
-}
