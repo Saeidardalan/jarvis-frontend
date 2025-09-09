454ae293f725712c556e16adf2a519f54149dec0
Saeidardalan <saeidardalan2@gmail.com>
2025-09-09 13:59:42 +0330

    Delete src/src/hooks/src/hooks/use-toast.ts

--- src/src/hooks/src/hooks/use-toast.ts
+++
@@ -1,8 +0,0 @@
-// src/lib/utils.ts
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-// Utility function to merge Tailwind classes
-export function cn(...inputs: (string | undefined | null | boolean)[]) {
-  return twMerge(clsx(inputs));
-}
